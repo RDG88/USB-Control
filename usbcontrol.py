165b608e75ee2be99573f98e70c63b7d77af3e74
Rob de Graaf <40949874+RDG88@users.noreply.github.com>
2023-11-26 14:32:36 +0100

    Update usbcontrol.py

--- usbcontrol.py
+++ usbcontrol.py
@@ -65,6 +65,44 @@ atexit.register(Disconnect, service_instance)
 
 content = service_instance.RetrieveContent()
 
+def add_usb_3_2_controller(vm):
+    # Check if the required USB controller already exists
+    for device in vm.config.hardware.device:
+        if isinstance(device, vim.vm.device.VirtualUSBXHCIController):  # Assuming this is the class for USB 3.2
+            print("USB 3.2 Controller already exists on VM:", vm.name)
+            return
+
+    # Create a new USB 3.2 controller
+    usb_controller = vim.vm.device.VirtualUSBXHCIController()  # Assuming this is the class for USB 3.2
+    usb_controller.busNumber = 0
+    usb_controller.key = -101  # Unique key, different from USB 2.0 controller
+    usb_controller.deviceInfo = vim.Description()
+    usb_controller.deviceInfo.label = 'USB 3.2 Controller'
+    usb_controller.deviceInfo.summary = 'USB 3.2 Controller'
+
+    # Create the Virtual Device Spec
+    spec = vim.vm.ConfigSpec()
+    dev_changes = []
+    dev_spec = vim.vm.device.VirtualDeviceSpec()
+    dev_spec.operation = vim.vm.device.VirtualDeviceSpec.Operation.add
+    dev_spec.device = usb_controller
+    dev_changes.append(dev_spec)
+
+    spec.deviceChange = dev_changes
+    task = vm.ReconfigVM_Task(spec=spec)
+
+    # Wait for the task to complete
+    while task.info.state == vim.TaskInfo.State.running:
+        time.sleep(1)
+
+    if task.info.state == vim.TaskInfo.State.error:
+        print("Error adding USB 3.2 Controller to VM:", vm.name)
+        if task.info.error:
+            print(task.info.error)
+        return False
+    return True
+
+
 
 def add_usb_controller(vm):
     # Check if a USB controller already exists
@@ -186,7 +224,10 @@ for host in hosts_in_datacenter:
             raise SystemExit(f"VM '{args.vm_name}' not found")
 
         # Add USB 2.0 Controller
-        add_usb_controller(selected_vm)
+        # add_usb_controller(selected_vm)
+        # Add USB 3.2 Controller
+        add_usb_3_2_controller(selected_vm)
+        
 
         for device in selected_vm.config.hardware.device:
             if isinstance(device, vim.vm.device.VirtualUSB):
